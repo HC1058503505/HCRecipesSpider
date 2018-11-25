ebda0fda19764ae1c254abad8c6a78defaf508a7
HongCong <1058503505@qq.com>
2018-11-25 22:10:47 +0800

    完善

--- HCRecipes/spiders/recommendedDaily.py
+++ HCRecipes/spiders/recommendedDaily.py
@@ -1,3 +1,5 @@
+# ----- coding:utf-8 --------
+
 import re
 import requests
 import scrapy
@@ -25,10 +27,41 @@ class RecommendedDaily(scrapy.Spider):
 	 		yield Request(cover_p_retur_href, self.recipesDetail)
 	 		break
 
+
 	def recipesDetail(self, response):
 		recipesDetail_bs = BeautifulSoup(response.text,'lxml')
-		div_bokpic = recipesDetail_bs.find('div', class_='bokpic')
+		recipe_info = recipesDetail_bs.find('div', class_='recinfo')
+		div_bokpic = recipe_info.find('div', class_='bokpic')
 		div_bokpic_a = div_bokpic.find('a')
 		div_bokpic_a_img = div_bokpic_a.find('img')
-		print div_bokpic_a_img['href']
-		
+		# 图片
+		div_bokpic_a_img_src = ['src']
+		# 标题
+		h1 = recipe_info.find('h1',id='page_cm_id')
+		recipe_title = h1.text.strip()
+		# 浏览搜藏
+		falisc_div = recipe_info.find('div', class_='falisc')
+		falisc_scan = falisc_div.find('span', class_='collectview')
+		falisc_collection = falisc_div.find('span',class_='collectnum')
+		# 材料
+		retew_div = recipe_info.find('div',class_='retew')
+		retew_div_table = retew_div.find('table',class_='retamr')
+		retew_div_table_trs = retew_div_table.find_all('tr')
+		for x in xrange(0,len(retew_div_table_trs)):
+			print x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
