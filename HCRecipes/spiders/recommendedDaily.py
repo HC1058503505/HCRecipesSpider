adaacf697284f6ad22c94d2e1fb3a0b7a02199a6
HongCong <1058503505@qq.com>
2018-11-26 23:41:59 +0800

    完善菜谱爬虫，材料以及步骤

--- HCRecipes/spiders/recommendedDaily.py
+++ HCRecipes/spiders/recommendedDaily.py
@@ -48,12 +48,52 @@ class RecommendedDaily(scrapy.Spider):
 		retew_div_table = retew_div.find('table',class_='retamr')
 		retew_div_table_trs = retew_div_table.find_all('tr')
 		
+
 		zfliao_table = []
 		zfliao_table_slices = self.sliceMtims(retew_div_table_trs)
-		print len(zfliao_table_slices)
-		for slcie in zfliao_table_slices:
-			pass
 
+		for slcie_trs in zfliao_table_slices:
+
+			rows_list = []
+			sections = []
+			for i in range(len(slcie_trs)):
+				tr = slcie_trs[i]
+				if i == 0:
+					section_title = tr.get_text('|', strip=True)
+					sections = self.itemMtims(section_title)
+				else:
+					rows_text = tr.get_text('|', strip=True)
+					rows = self.itemMtims(rows_text)
+					rows_list = rows_list + rows
+
+			section_dic = {'section_title' : sections, 'rows_list' : rows_list}
+			zfliao_table.append(section_dic)
+
+		# 做菜步骤
+		recipes_steps = []
+		retew_div_steps = retew_div.find('div',class_='step')
+
+		retew_div_stepconts = retew_div_steps.find_all('div',class_='stepcont')
+
+		
+		for stepcont in retew_div_stepconts:
+
+			stepcont_pldc = stepcont.find('div',class_='pldc')
+			stepcont_pldc_a = stepcont_pldc.find('a')
+			# 步骤图片href
+			stepcont_pldc_a_href = stepcont_pldc_a['href']
+			# 步骤文字介绍
+			stepcont_pldc_p = stepcont.find('p')
+
+			stepcont_pldc_p_text = stepcont_pldc_p.get_text(strip=True)
+			recipes_steps.append({
+					'step_img_href' : stepcont_pldc_a_href,
+					'step_text' : stepcont_pldc_p_text
+				})
+
+
+			
+		print '-------------------------------------------------------------'
 
 	def sliceMtims(self,tbody_bs_tr):
 		mtim = ()
@@ -75,8 +115,15 @@ class RecommendedDaily(scrapy.Spider):
 
 		return results
 
-
-
+	def itemMtims(self,item):
+		params = item.split('|')
+		params_count = len(params)
+		if params_count == 1:
+			return [{"item_title" : params[0], "item_value" : ""}]
+		elif params_count == 2:
+			return [{"item_title" : params[0], "item_value" : params[-1]}]
+		elif params_count == 4:
+			return [{"item_title" : params[0], "item_value" : params[1]},{"item_title" : params[2], "item_value" : params[3]}]
 
 
 
